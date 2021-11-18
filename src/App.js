281623f7cf971e2547cd884478e447eb49293dec
devoris <oussam92.ing@gmail.com>
2021-11-18 09:56:27 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import image1 from "./images/ImagePr.jpg";
+import "./Css/style.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      Person: {
+        FullName: "oussama ",
+        bio: "",
+        profession: "",
+        imgsrc: { image1 },
+        isActive: false,
+      },
+    };
+  }
+  handleShow = () => {
+    this.setState({
+      isActive: true,
+    });
+  };
+  handleHide = () => {
+    this.setState({
+      isActive: false,
+    });
+  };
+  render() {
+    const styleObject = { color: "grey", textAlign: "center" };
+    if (this.state.isActive) {
+      return (
+        <div style={styleObject}>
+          <div className="btn">
+            <button onClick={this.handleHide}>Hide</button>
+          </div>
+          <div className="avatar">
+            <img src={image1} alt="me" />
+          </div>
+          <h1>{this.state.Person.FullName}</h1>
+          <h2>{this.state.Person.profession}</h2>
+          <p>{this.state.Person.bio}</p>
+        </div>
+      );
+    } else {
+      return (
+        <div className="btn">
+          <button onClick={this.handleShow}>Show</button>
+        </div>
+      );
+    }
+  }
 }
-
 export default App;
